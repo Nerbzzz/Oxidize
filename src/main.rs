659a4c001e1849d366c104a1ea3ec1ede7a717e6
Nerbzzz <briandulkenraad2004@gmail.com>
2025-04-05 16:55:48 +0200

    Add healthcheck and setup tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use serde_json::json;
+use tracing_subscriber;
+
+pub async fn health_check() -> impl IntoResponse {
+    (StatusCode::OK, Json(json!({
+        "status": u16::from(StatusCode::OK),
+        "message": "OK"
+    })))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/health", get(health_check));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000)); 
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+
+    axum::serve(listener, app).await?;
+    Ok(())
 }
